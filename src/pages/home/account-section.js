5e0357ea384c9814e5186059fd5dd362aeb4d3a6
i-havr <i.havrylov@gmail.com>
2025-02-22 16:37:09 +0200

    feat/add acc section start mockup

---
+++ src/pages/home/account-section.js
@@ -0,0 +1,10 @@
+export default function AccountSection() {
+  return `
+    <section class="mx-auto text-center">
+        <div class="container">
+            <div class="flex flex-col bg-bgSecondary py-4 md:py-5 xl:py-6 px-4 md:px-[26px] xl:px-8 rounded-xl">
+            
+            </div>
+        </div>
+    </section>`;
+}
