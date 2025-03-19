eb2e8b24707fb348400bb047400aefdde953d4a4
i-havr <i.havrylov@gmail.com>
2025-03-19 14:01:32 +0200

    fix(vite): fix gh pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/fl-ui/",
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  }
 });
